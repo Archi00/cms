7b8fb89cca81eba29c5b10bc208f9a277a078f5d
Archi00 <albert.archi@gmail.com>
2020-07-08 22:59:39 +0200

    Added links to main html fetching rows from the database

--- index.php
+++ index.php
@@ -1,9 +1,6 @@
-<?php
+<?php include "includes/db.php"; ?>
+<?php include "includes/header.php"; ?>
 
-include "includes/header.php";
-
- ?>
- <!DOCTYPE html>
     <!-- Navigation -->
 <?php include "includes/navigation.php" ?>
 
@@ -35,59 +32,13 @@ include "includes/header.php";
                 <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
 
                 <hr>
-
-                <!-- Second Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam, quasi, fugiat, asperiores harum voluptatum tenetur a possimus nesciunt quod accusamus saepe tempora ipsam distinctio minima dolorum perferendis labore impedit voluptates!</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-
-                <!-- Third Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:45 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, voluptates, voluptas dolore ipsam cumque quam veniam accusantium laudantium adipisci architecto itaque dicta aperiam maiores provident id incidunt autem. Magni, ratione.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-
-                <hr>
-
-                <!-- Pager -->
-                <ul class="pager">
-                    <li class="previous">
-                        <a href="#">&larr; Older</a>
-                    </li>
-                    <li class="next">
-                        <a href="#">Newer &rarr;</a>
-                    </li>
-                </ul>
-
-            </div>
-
+              </div>
             <!-- Blog Sidebar Widgets Column -->
 
-          <?php include "includes/sidebar.php" ?>
+          <?php include "includes/sidebar.php"; ?>
 
         </div>
         <!-- /.row -->
 
         <hr>
-
-
-<?php include "/includes/footer.php" ?>
+<?php include "includes/footer.php"; ?>

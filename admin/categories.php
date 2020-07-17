89828fe6ea1d12bc6ac9d16494ffefe31fec040d
Archi00 <albert.archi@gmail.com>
2020-07-17 21:50:12 +0200

    Created categories.php, and renamed the includes in admin.

---
+++ admin/categories.php
@@ -0,0 +1,69 @@
+<?php include "includes/admin_header.php"; ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+
+        <?php include "includes/admin_navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+
+                        <h1 class="page-header">
+                            Blank Page
+                            <small>Subheading</small>
+                        </h1>
+
+                        <!--Add category form-->
+                        <div class="col-xs-6">
+                          <form class="" action="index.html" method="post">
+                            <div class="form-group">
+                              <label for="cat_title"> Add Category </label>
+                              <input class="form-control" type="text" name="cat_title">
+                            </div>
+                            <div class="form-group">
+                              <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
+                            </div>
+
+                          </form>
+                        </div>
+                        <!--#Add category form-->
+
+                      <div class="col-xs-6">
+                        <table class="table table-bordered table-hover">
+                          <thead>
+                            <tr>
+                              <th> ID </th>
+                              <th> Category Title </th>
+                            </tr>
+                          </thead>
+                          <tbody>
+                            <tr>
+                              <td>Test Category 1</td>
+                              <td>Test Category 2</td>
+                            </tr>
+                          </tbody>
+                        </table>
+
+
+                      </div>
+
+                      </div>
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+
+        <!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.php"; ?>

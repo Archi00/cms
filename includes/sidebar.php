21ac295fa16685b45aa983d7e9b5cd989a0398fa
Archi00 <albert.archi@gmail.com>
2020-07-17 20:24:49 +0200

    Made a new file just for the widget, cleaned sidebar a bit.

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -28,7 +28,7 @@
                   $select_categories_sidebar = mysqli_query($connection,$query);
               ?>
 
-            <div class="col-lg-6">
+            <div class="col-lg-12">
                 <ul class="list-unstyled">
 
                   <?php
@@ -43,28 +43,10 @@
 
                 </ul>
             </div>
-            <!-- /.col-lg-6 -->
-            <div class="col-lg-6">
-                <ul class="list-unstyled">
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                    <li><a href="#">Category Name</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.col-lg-6 -->
         </div>
         <!-- /.row -->
     </div>
 
-    <!-- Side Widget Well -->
-    <div class="well">
-        <h4>Side Widget Well</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-    </div>
+    <?php include "widget.php"; ?>
 
 </div>

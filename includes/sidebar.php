4244383bddc6035cdcd975e6250a3dc70636b61b
Archi00 <albert.archi@gmail.com>
2020-07-17 19:58:19 +0200

    Search bar now working, redirecting to search.php using the tags row.

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,14 +3,18 @@
     <!-- Blog Search Well -->
     <div class="well">
         <h4>Blog Search</h4>
+        <form action="search.php" method="post">
+
+
         <div class="input-group">
-            <input type="text" class="form-control">
+            <input name="search" type="text" class="form-control">
             <span class="input-group-btn">
-                <button class="btn btn-default" type="button">
+                <button name="submit" class="btn btn-default" type="submit">
                     <span class="glyphicon glyphicon-search"></span>
             </button>
             </span>
         </div>
+      </form> <!-- search form -->
         <!-- /.input-group -->
     </div>
 

00dea80b0142760a53fd9ec005b61c0955b9cda0
Archi00 <albert.archi@gmail.com>
2020-07-17 14:16:24 +0200

    Added the main things to the cms database, and currently is shown dinamicly on the webpage.

--- includes/db.php
+++ includes/db.php
@@ -6,10 +6,11 @@ $db['db_pass'] = "";
 $db['db_name'] = "cms";
 
 foreach($db as $key => $value) {
-define(strtoupper($key), $value);
+    define(strtoupper($key), $value);
 }
 
 $connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
 if(!$connection) {
 
   die("fucked it up, huh...");

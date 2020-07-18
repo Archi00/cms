c77850efe809ef676aba2bbc08eb2b8777126a28
Archi00 <albert.archi@gmail.com>
2020-07-18 19:37:29 +0200

    Added the posts.php, can see all the posts from the admin area.

---
+++ admin/posts.php
@@ -0,0 +1,88 @@
+<?php include "includes/admin_header.php"; ?>
+
+    <div id="wrapper">
+
+        <!-- Navigation -->
+
+        <?php include "includes/admin_navigation.php"; ?>
+
+        <div id="page-wrapper">
+
+            <div class="container-fluid">
+
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+
+                        <h1 class="page-header">
+                            Welcome to Admin
+                            <small>Archi00</small>
+                        </h1>
+
+                        <table class="table table-bordered table-hover">
+                          <thead>
+                            <tr>
+                              <th>ID</th>
+                              <th>Author</th>
+                              <th>Title</th>
+                              <th>Category</th>
+                              <th>Status</th>
+                              <th>Image</th>
+                              <th>Tags</th>
+                              <th>Comments</th>
+                              <th>Date</th>
+                            </tr>
+                          </thead>
+                          <tbody>
+
+                           <?php
+
+                            $query = "SELECT * FROM posts";
+                            $select_all_posts = mysqli_query($connection,$query);
+
+                            while($row = mysqli_fetch_assoc($select_all_posts)){
+                                $post_id = $row['post_id'];
+                                $post_author = $row['post_author'];
+                                $post_title = $row['post_title'];
+                                $post_category_id = $row['post_category_id'];
+                                $post_status = $row['post_status'];
+                                $post_image = $row['post_image'];
+                                $post_tags = $row['post_tags'];
+                                $post_comment_count = $row['post_comment_count'];
+                                $post_date = $row['post_date'];
+                            ?>
+
+                            <tr>
+                              <?php
+
+                                echo "<td>{$post_id}</td>";
+                                echo "<td>{$post_author}</td>";
+                                echo "<td>{$post_title}</td>";
+                                echo "<td>{$post_category_id}</td>";
+                                echo "<td>{$post_status}</td>";
+                                echo "<td>{$post_image}</td>";
+                                echo "<td>{$post_tags}</td>";
+                                echo "<td>{$post_comment_count}</td>";
+                                echo "<td>{$post_date}</td>";
+
+                              ?>
+                            </tr>
+
+                          <?php } ?>
+
+                          </tbody>
+                        </table>
+                      </div>
+
+                    </div>
+                </div>
+                <!-- /.row -->
+
+            </div>
+            <!-- /.container-fluid -->
+
+        </div>
+
+        <!-- /#page-wrapper -->
+
+<?php include "includes/admin_footer.php"; ?>

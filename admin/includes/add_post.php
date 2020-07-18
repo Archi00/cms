b20de305c8dd8f54aff5bd8d7eb0b9d6513ea7f6
Archi00 <albert.archi@gmail.com>
2020-07-19 01:10:56 +0200

    Added the function to select categories, and display them.

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -5,7 +5,7 @@ if(isset($_POST['create_post'])) {
 
   $post_title = $_POST['title'];
   $post_author = $_POST['author'];
-  $post_category_id = $_POST['post_category_id'];
+  $post_category_id = $_POST['post_category'];
   $post_status = $_POST['post_status'];
 
   $post_image = $_FILES['image']['name'];
@@ -43,8 +43,28 @@ if(isset($_POST['create_post'])) {
   </div>
 
   <div class="form-group">
-    <label for="post_category"> Post Category ID </label>
-      <input type="text" class="form-control" name="post_category_id">
+
+    <select  name="post_category">
+
+      <?php
+
+      $query = "SELECT * FROM categories";
+      $select_categories = mysqli_query($connection,$query);
+
+      confirm($select_categories);
+
+      while($row = mysqli_fetch_assoc($select_categories)){
+            $cat_id = $row['cat_id'];
+            $cat_title = $row['cat_title'];
+
+            echo "<option value='{$cat_id}'>{$cat_title}</option>";
+      }
+       ?>
+
+    </select>
+
+
+
   </div>
 
   <div class="form-group">

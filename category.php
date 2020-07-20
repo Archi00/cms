b9a97cb7b558a837bba118ec6bbedc95144649c6
Archi00 <albert.archi@gmail.com>
2020-07-20 21:09:11 +0200

    Added links for the categories.

---
+++ category.php
@@ -0,0 +1,71 @@
+<?php include "includes/db.php"; ?>
+<?php include "includes/header.php"; ?>
+
+    <!-- Navigation -->
+<?php include "includes/navigation.php" ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+            <div class="col-md-8">
+
+                <?php
+
+                if(isset($_GET['category'])) {
+
+                  $the_category = $_GET['category'];
+
+
+                }
+
+                  $query = "SELECT * FROM posts WHERE post_category_id = $the_category ";
+                  $select_all_posts = mysqli_query($connection,$query);
+
+
+                  while($row = mysqli_fetch_assoc($select_all_posts)){
+                      $post_id = $row['post_id'];
+                      $post_title = $row['post_title'];
+                      $post_author = $row['post_author'];
+                      $post_date = $row['post_date'];
+                      $post_image = $row['post_image'];
+                      $post_comment = $row['post_comment'];
+                ?>
+
+                <h1 class="page-header">
+                    Page Heading
+                    <small>Secondary Text</small>
+                </h1>
+
+                <!-- First Blog Post -->
+                <h2>
+                    <a href="post.php?p_id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a>
+                </h2>
+                <p class="lead">
+                    by <a href=""><?php echo $post_author ?></a>
+                </p>
+                <p><span class="glyphicon glyphicon-time"></span><?php echo $post_date; ?></p>
+                <hr>
+                <img class="img-responsive" src=<?php echo "images/{$post_image}"; ?>>
+                <hr>
+                <p><?php echo $post_comment; ?></p>
+                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+
+                <hr>
+
+              <?php } ?>
+
+              </div>
+
+
+            <!-- Blog Sidebar Widgets Column -->
+
+          <?php include "includes/sidebar.php"; ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+<?php include "includes/footer.php"; ?>
